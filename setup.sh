a3d22e5f9fb51380a0b042decc2e72ee662f1922
Giona2 <153511987+Giona2@users.noreply.github.com>
2024-04-01 22:29:35 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 echo "This setup bash script will install:"
 echo "- nala\n- python3\n- python3-tk\n- openbox"
-echo "Continue (Y/n)? "
+echo "\nContinue (Y/n)? "
 read confirmed_install
 
 if [ $confirmed_install=="n" ]; then
@@ -11,9 +11,10 @@ elif [ $confirmed_install!="Y" ]; then
     read $confirmed_install
 fi
 
-sudo apt update
-sudo apt upgrade
+sudo apt -y update
+sudo apt -y upgrade
 sudo apt install -y nala
+
 sudo nala install -y python3
 sudo nala install -y python3-tk
 sudo nala install -y openbox
