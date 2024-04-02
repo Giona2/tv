472ba94993d85af10e31d60efc528a37ec48d725
Giona2 <153511987+Giona2@users.noreply.github.com>
2024-04-02 12:54:54 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ if [ "$arg" == "--help" ] || [ "$arg" == "-h" ]; then
     echo -e "\n./setup.sh <command> <argument>"
     echo
     echo "Commands"
-    echo "========================================================================================"
+    echo "=================================================================================================="
     echo "./setup.sh -h/--help                    prints the documentation for all commands for this program"
     echo -e "./setup.sh -s/--start                   begins the setup bash script\n"
     exit 0
@@ -17,13 +17,12 @@ fi
 echo "This setup script will install"
 echo -e "- nala\n- python3\n- python3-tk\n- xorg\n- xterm"
 echo -n "Do you wish to continue? [Y/n] "
-read confirmed_install
-while [ "$confirmed_install" != "Y" ] && [ "$confirmed_install" != "y" ]; do
+while read confirmed_install; do
 if [ "$confirmed_install" == "N" ] || [ "$confirmed_install" == "n" ]; then
     echo "Cancelled..."
     exit 0
-elif [ "$confirmed_install" != "Y" ] && [ "$confirmed_install" != "y" ]; then
-    read $confirmed_install
+elif [ "$confirmed_install" == "Y" ] || [ "$confirmed_install" == "y" ]; then
+    break
 fi
 done
 
