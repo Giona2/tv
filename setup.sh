fd96c17f4c5ef87cb540c26573faffbcf85a3494
Giona2 <153511987+Giona2@users.noreply.github.com>
2024-04-02 12:25:44 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,10 +4,12 @@ arg=$1
 echo $arg
 
 if [ "$arg" == "--help" ] || [ "$arg" == "-h" ]; then
-    echo "./setup.sh <command> <argument>"
+    echo -e "\n./setup.sh <command> <argument>"
+    echo
+    echo "Commands"
     echo "========================================================================================"
     echo "./setup.sh -h/--help                    prints the documentation for all commands for this program"
-    echo "./setup.sh -s/--start                   begins the setup bash script"
+    echo -e "./setup.sh -s/--start                   begins the setup bash script\n"
     exit 0
 elif [ "$arg" != "--start" ] && [ "$arg" != "-s" ]; then
     exit 0
