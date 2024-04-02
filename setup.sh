065610dd432c7f1afe76c5eb098a6b1aa28172d7
Giona2 <153511987+Giona2@users.noreply.github.com>
2024-04-02 12:33:22 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,6 +14,19 @@ elif [ "$arg" != "--start" ] && [ "$arg" != "-s" ]; then
     exit 0
 fi
 
+echo "This setup script will install"
+echo -e "- nala\n- python3\n- python3-tk\n- xorg\n- xterm"
+echo -n "Do you wish to continue? [Y/n] "
+read confirmed_install
+while [ "$confirmed_install" != "Y" ] && [ "$confirmed_install" != "y" ]; do
+if [ "$confirmed_install" == "N" ] || [ "$confirmed_install" == "n" ]; then
+    echo "Cancelled..."
+    exit 0
+elif [ "$confirmed_install" != "Y" ] && [ "$confirmed_install" != "y" ]; then
+    read $confirmed_install
+fi
+done
+
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt install -y nala
