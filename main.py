a2978745deaea45b4823806b05e3d412d6e220ad
Giona2 <153511987+Giona2@users.noreply.github.com>
2024-04-05 13:29:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import *
 root = Tk()
 root.geometry("1920x1080")
 
-exit_btn = Button(root, text="exit", command=lambda: exit())
+exit_btn = Button(root, text="exit", command=exit)
 exit_btn.pack()
 
 
